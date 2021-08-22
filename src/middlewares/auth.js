6f9883b9b9decad85a83a64a42c22c61abf94e8f
Mathius Kormasela <mathiuskormasela12@gmail.com>
2021-08-22 14:36:40 +0700

    done authorization

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,8 @@
 // ========== Auth Middleware
 // import all modules
 const { body, validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
 const response = require('../helpers/response')
 
 exports.checkRegisterForm = [
@@ -47,3 +49,45 @@ exports.checkLoginForm = [
     return next()
   }
 ]
+
+exports.isCompany = (req, res, next) => {
+  const token = req.headers.authorization
+
+  if (token) {
+    jwt.verify(token, config.secret_key, (err, decode) => {
+      if (err) {
+        return response(req, res, 400, false, err.message)
+      } else {
+        if (decode.role === 1) {
+          req.data = decode
+          return next()
+        } else {
+          return response(req, res, 403, false, 'Forbidden')
+        }
+      }
+    })
+  } else {
+    return response(req, res, 403, false, 'Forbidden')
+  }
+}
+
+exports.isVendor = (req, res, next) => {
+  const token = req.headers.authorization
+
+  if (token) {
+    jwt.verify(token, config.secret_key, (err, decode) => {
+      if (err) {
+        return response(req, res, 400, false, err.message)
+      } else {
+        if (decode.role === 0) {
+          req.data = decode
+          return next()
+        } else {
+          return response(req, res, 403, false, 'Forbidden')
+        }
+      }
+    })
+  } else {
+    return response(req, res, 403, false, 'Forbidden')
+  }
+}
